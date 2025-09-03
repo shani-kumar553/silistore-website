761579f0f873caaeae27ac1d6c205e5cd3584852
shani-kumar553 <shanikumarsunny03@gmail.com>
2025-09-03 16:45:40 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+const products = [
+  {
+    id: 1,
+    name: "Silicone Zip Storage Bag – 500ml",
+    price: "₹249",
+    img: "apni-product-image-link",
+    tags: ["Leak-proof", "Reusable", "Food-grade"],
+  },
+  ...
+];
